51928958015b853963511ceecba751f2d9916661
Egba Frank <Egbafrank72@gmail.com>
2024-10-27 19:40:31 +0000

    Add paginators

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Contains a simple paginator
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    Calculate page index range
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return (start, end)
